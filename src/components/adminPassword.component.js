a61dc8dad85d34c9391acbd39d542eb899c149ec
Kenzie Tee <teekenzie@berkeley.edu>
2021-04-04 16:04:08 -0700

    implemented password page for admin dashboard

--- src/components/adminPassword.component.js
+++ src/components/adminPassword.component.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 export const AdminPassword = ({ setToken }) => {
   const [password, setPassword] = useState();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const token = 'tempToken'; // change this to a request for generate token
-    setToken(token);
+    const loginReq = await axios.post('http://localhost:8080/adminVerify', {
+      password: password,
+    });
+    if (loginReq && loginReq.status === 200) {
+      // change this to a request for generate token
+      const token = loginReq.data.token;
+      setToken(token);
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      return;
+    } else {
+      alert('Password is incorrect!');
+    }
   };
 
   return (
@@ -17,7 +28,7 @@ export const AdminPassword = ({ setToken }) => {
           <p>Password</p>
           <input
             type="password"
-            onChange={setPassword}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="Admin Password"
           />
         </label>
