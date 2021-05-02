a4abc6eb74f093b0fa57359c2ab679f9003ef524
MikePodo <72575260+MikePodo@users.noreply.github.com>
2021-05-01 17:08:50 -0700

    create container add listing UI

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Modal from "@material-ui/core/Modal";
 
 import RemoveIcon from "@material-ui/icons/Remove";
 import AddIcon from "@material-ui/icons/Add";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
 
 import containerService from "../services/containerService";
@@ -10,6 +11,9 @@ import containerService from "../services/containerService";
 const Container = ({ title, containers, setContainers, initialListings }) => {
   const [confirmDelete, setConfirmDelete] = useState(false);
   const [listings, setListings] = useState(initialListings);
+  const [addListingMode, setAddListingMode] = useState(false);
+
+  const listingInput = useRef(null);
 
   const handleDeleteContainer = async () => {
     await containerService.deleteContainer(title);
@@ -20,6 +24,15 @@ const Container = ({ title, containers, setContainers, initialListings }) => {
   const handleRemoveListing = async (listing) => {
     await containerService.deleteListing(title, listing);
     setListings(listings.filter((l) => l !== listing));
+  };
+
+  const handleAddListingMode = () => {
+    setAddListingMode(!addListingMode);
+    setTimeout(() => listingInput.current && listingInput.current.focus(), 50);
+  };
+
+  const handleAddListing = async () => {
+    console.log("test");
   };
 
   return (
@@ -46,7 +59,10 @@ const Container = ({ title, containers, setContainers, initialListings }) => {
       <div className="container-header">
         <h2>{title}</h2>
         <div>
-          <AddIcon className="add-listing-container-icon" />
+          <AddIcon
+            className="add-listing-container-icon"
+            onClick={handleAddListingMode}
+          />
           <DeleteOutlineIcon
             onClick={() => {
               setConfirmDelete(true);
@@ -56,6 +72,17 @@ const Container = ({ title, containers, setContainers, initialListings }) => {
         </div>
       </div>
       <div className="container-listings-container">
+        {addListingMode && (
+          <div className="container-add-listing-input-container">
+            <input
+              type="text"
+              className="container-add-listing-input"
+              ref={listingInput}
+              placeholder="Listing ID"
+            />
+            <AddCircleIcon onClick={handleAddListing} />
+          </div>
+        )}
         {listings.map((listing) => (
           <div className="container-listing" key={listing}>
             <a
