aa8d629053e14f0b6d5f88d48529bc9b1bb48b38
MikePodo <72575260+MikePodo@users.noreply.github.com>
2021-05-25 22:59:05 -0700

    add function to parse housekeeping users data #16

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,12 @@
 import React, { useState, useEffect } from "react";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
 
 import Navbar from "../components/Navbar";
 import houseKeepingService from "../services/houseKeepingService";
@@ -6,15 +14,52 @@ import houseKeepingService from "../services/houseKeepingService";
 const Dashboard = () => {
   const [data, setData] = useState([]);
 
+  // const data = [
+  //   {
+  //     date: "5/19",
+  //     users: 23,
+  //   },
+  //   {
+  //     date: "5/20",
+  //     users: 25,
+  //   },
+  //   {
+  //     date: "5/21",
+  //     users: 30,
+  //   },
+  //   {
+  //     date: "5/22",
+  //     users: 31,
+  //   },
+  //   {
+  //     date: "5/23",
+  //     users: 35,
+  //   },
+  //   {
+  //     date: "5/24",
+  //     users: 37,
+  //   },
+  //   {
+  //     date: "5/25",
+  //     users: 39,
+  //   },
+  // ];
+
   useEffect(() => {
     const getData = async () => {
-      const res = await houseKeepingService.getUsersData();
-      setData(res.data.payload);
+      const dataObject = await houseKeepingService.getUsersData();
+      let dataArray = [];
+      for (const [key, value] of Object.entries(dataObject.data.payload)) {
+        dataArray.push({
+          date: key,
+          Users: value,
+        });
+      }
+
+      setData(dataArray);
     };
     getData();
   }, []);
-
-  console.log(data);
 
   return (
     <div className="dashboard-screen">
@@ -23,7 +68,15 @@ const Dashboard = () => {
         <div className="dashboard-container-header">
           <h1>Dashboard</h1>
         </div>
-        <div className="dashboard-content"></div>
+        <div className="dashboard-content">
+          <LineChart width={1000} height={400} data={data}>
+            <Line type="monotone" dataKey="Users" stroke="#00b183" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="date" />
+            <YAxis dataKey="Users" />
+            <Tooltip />
+          </LineChart>
+        </div>
       </div>
     </div>
   );
